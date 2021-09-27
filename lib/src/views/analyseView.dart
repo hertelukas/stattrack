4f9ccc35d0436854be0dfb3204259d09d2cd96c1
hertelukas <luki.hertel@gmail.com>
2021-09-27 18:18:04 +0200

    Set a range of dates to show

--- lib/src/views/analyseView.dart
+++ lib/src/views/analyseView.dart
@@ -1,5 +1,6 @@
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:stattrack/src/business_logic/models/data.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -12,6 +13,7 @@ class _AnalyseViewState extends State<AnalyseView> {
   String key = "";
   List<charts.Series<Entry, DateTime>> series = List.empty();
   List<String> keys = List.empty();
+  DateTimeRange? timeRange;
 
   _AnalyseViewState() {
     keys = Data.singleton.getNumKeys().toList();
@@ -19,7 +21,7 @@ class _AnalyseViewState extends State<AnalyseView> {
       return;
     }
     key = keys[0];
-    series = _convertToChart(Data.singleton.entries, key);
+    series = _convertToChart(timeRange, key);
   }
 
   @override
@@ -27,25 +29,44 @@ class _AnalyseViewState extends State<AnalyseView> {
     if (keys.isEmpty) {
       return Center(child: Text(AppLocalizations.of(context)!.no_fields));
     }
+
+    DateFormat dateFormat =
+        DateFormat.yMEd(AppLocalizations.of(context)!.localeName);
+
     return ListView(
       padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 0),
       children: [
-        Center(
-            child: DropdownButton<String>(
-                value: key,
-                items: keys.map<DropdownMenuItem<String>>((String tempKey) {
-                  return DropdownMenuItem<String>(
-                    value: tempKey,
-                    child: Text(tempKey),
-                  );
-                }).toList(),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    key = newValue!;
-                    print("key is " + key);
-                    series = _convertToChart(Data.singleton.entries, key);
-                  });
-                })),
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          DropdownButton<String>(
+              value: key,
+              items: keys.map<DropdownMenuItem<String>>((String tempKey) {
+                return DropdownMenuItem<String>(
+                  value: tempKey,
+                  child: Text(tempKey),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  key = newValue!;
+                  series = _convertToChart(timeRange, key);
+                });
+              }),
+          TextButton(
+              onPressed: () async {
+                timeRange = await showDateRangePicker(
+                    context: context,
+                    firstDate: DateTime(2020),
+                    lastDate: DateTime(2100));
+                setState(() {
+                  series = _convertToChart(timeRange, key);
+                });
+              },
+              child: Text(timeRange == null
+                  ? AppLocalizations.of(context)!.set_range
+                  : dateFormat.format(timeRange!.start) +
+                      " - " +
+                      dateFormat.format(timeRange!.end)))
+        ]),
         Container(
             padding: const EdgeInsets.all(8),
             height: MediaQuery.of(context).size.height / 3,
@@ -59,14 +80,25 @@ class _AnalyseViewState extends State<AnalyseView> {
   }
 
   static List<charts.Series<Entry, DateTime>> _convertToChart(
-      List<Entry> entries, String key) {
-    print("Looking up for " + key);
+      DateTimeRange? range, String key) {
+    List<Entry> entries;
+    if (range == null) {
+      entries = Data.singleton.entries;
+    } else {
+      entries = Data.singleton.getBetween(range.start, range.end);
+    }
     return [
       new charts.Series<Entry, DateTime>(
           id: 'History',
           colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
           domainFn: (Entry entry, _) => entry.date,
-          measureFn: (Entry entry, _) => entry.fields[key] as num,
+          measureFn: (Entry entry, _) {
+            if (entry.fields[key] is num) {
+              return entry.fields[key] as num;
+            } else {
+              return null;
+            }
+          },
           data: entries)
     ];
   }
